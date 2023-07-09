4021c3e49ba09472bda7ff665e7cafcea1e76a18
XaXhade <you@example.com>
2023-07-09 14:58:41 -0300

    terminado

--- App.js
+++ App.js
@@ -1,10 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import {useState, useEffect} from 'react'
+import Separator from './Separator';
 
 export default function App() {
+  const [text, onChangeText] = useState("")
+  const [textoAMostrar, setText] = useState("")
+  const [caracteres, setCaracteres] = useState(0)
+  const [seMuestra, setSeMuestra] = useState(false)
+
+  const mostrarTexto = () => {
+    setSeMuestra(true)
+    setText(text)
+  }
+
+  useEffect( () => {
+    setCaracteres(text.length)
+  }, [text])
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.box}>
+        <TextInput
+          style={styles.input} 
+          onChangeText={onChangeText}
+          value={text}
+          placeholder='ingrese texto'
+          
+        />
+        <Text style={styles.caracteres}>caracteres: {caracteres}</Text>
+        <Button 
+          title='Presioname'
+          style={styles.button}
+          onPress={mostrarTexto}
+        />
+        {seMuestra ? (
+          <View>
+            <Separator />
+            <Text style={styles.palabra}>{textoAMostrar}</Text>
+          </View>
+        ) : null}
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +51,31 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  box: {
+    borderColor: 'black',
+    borderWidth:  1,
+    padding: 10,
+  },
+  input: {
+    marginBottom: 10,
+    padding: 4,
+    borderWidth: 1, 
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  caracteres: {
+    textAlign: 'right',
+    color: 'red',
+  },
+  palabra: {
+    textAlign: 'center',
+  },
+  separator: {
+    height: 10,
+    backgroundColor: "red",
+    marginVertical: 1,
+    borderWidth: 4,
+    borderColor: 'black'
   },
 });
